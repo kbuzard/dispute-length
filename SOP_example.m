827f26f52973d21921a0bb52b4269d793ced07d2
kbuzard <kbuzard@gmail.com>
2015-12-18 13:52:25 -0500

    run example with no retaliation and add notes to outline

--- SOP_example.m
+++ SOP_example.m
@@ -1,5 +1,4 @@
-%Maintaining symmetry, I want to try a bigger intercept for the gamma
-%function PLUS probabilistic one-sided breaking (same gamma as lobby4).
+%Going to adapt SOP-RIE example for no retaliation
 
 %gamma: 1.25 + cn^.2
 %cn = .00166
@@ -8,19 +7,18 @@
 tn = (8*(1.25 + .00166^.2)-5)/(68-8*(1.25 + .00166^.2));
 
 a=.2;
-tns=tn;
 ta = [0:.001:.1259];
 tad = size(ta,2);
 c=[0:.00005:.005];
 tac = size(c,2);
 [ta,c]=meshgrid(ta,c);
 
-lhs = (8*(1.25 + (c).^a)-5).*(tn-ta) + (4*(1.25 + (c).^a)-34).*(tn^2-ta.^2)-3.*(tns-ta)+9.*(tns^2-ta.^2);
+lhs = (8*(1.25 + (c).^a)-5).*(tn-ta) + (4*(1.25 + (c).^a)-34).*(tn^2-ta.^2);
 
 %add e to gamma
 e1=.25; e2=.25;
-up = (8*(1.25 + (c).^a +e1)-5).*(tn-ta) + (4*(1.25 + (c).^a +e1)-34).*(tn^2-ta.^2)-3.*(tns-ta)+9.*(tns^2-ta.^2);
-low = (8*(1.25 + (c).^a -e2)-5).*(tn-ta) + (4*(1.25 + (c).^a -e2)-34).*(tn^2-ta.^2)-3.*(tns-ta)+9.*(tns^2-ta.^2);
+up = (8*(1.25 + (c).^a +e1)-5).*(tn-ta) + (4*(1.25 + (c).^a +e1)-34).*(tn^2-ta.^2);
+low = (8*(1.25 + (c).^a -e2)-5).*(tn-ta) + (4*(1.25 + (c).^a -e2)-34).*(tn^2-ta.^2);
 spread = up - low;
 b = up./spread;
 for j=1:tad
@@ -45,10 +43,9 @@ l4 = 4/49*((1+ta).^2);
 l5 = l3 + (1-l).*l4 - c;
 [M,I] = max(l5,[],1);
 opt_C = (I -1)/20000;
-%plot(TA,opt_C);
-
-
 TA = [0:.001:.125];
+plot(TA,opt_C);
+
 
 for j=1:tad
   B(j) = b(I(j),j);
@@ -60,7 +57,7 @@ E3 = E*E2;
 E4 = 21/49.*(2-2*TA.^2);
 E5 = (1-E).*E4;
 E6 = E3 + (1-E).*E4 ;
-%plot(TA,E6);
+plot(TA,E6);
 
 [M2,I2] = max(E6); %I2 gives E's choice of tau^a (63 ==> .062)
 ta_star = (I2 - 1)/1000
